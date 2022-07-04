af02d0f8bc64f577ea1496d63f90e4aea8f9468a
ikhlaas21 <ikhlaas21.rawoot@gmail.com>
2022-07-04 15:42:32 +0200

    4

--- Basic-Example/script/code.js
+++ Basic-Example/script/code.js
@@ -1,12 +1,25 @@
 // CREATE OBJECT
 const app = Vue.createApp({
 // CREATE VARIABLE IN DATA  
-     data(){
-
-     },
+    //this can also be done here instead of html
+     template:`
+     <input type='text' v-model= 'greeting'/>
+     <button v-on:click= 'display' >Click Me</button>
+     <div v-if='isVisible' id='box1'>
+     <p>{{greeting}}</p>
+     </div>
+     
+     `,
+     
      data: function() {
         return{
-            greeting:'ahoy'
+            greeting:'ahoy',
+            isVisible:'true'
+        }
+     },
+     methods:{
+        display(){
+           this.isVisible = !this.isVisible;
         }
      }
 //everything in here appears in the div with the app id
---
+++ form/script/code.js
@@ -0,0 +1,43 @@
+// CREATE OBJECT
+const app = Vue.createApp({
+    // CREATE VARIABLE IN DATA  
+        //this can also be done here instead of html
+         template:`
+         <form>
+         <input type='text' placeholder='name' v-model= 'FirstName' required>
+         <input type='text' placeholder='surname' v-model= 'LastName' required>
+         <input type='number' placeholder='age' v-model= 'Age' required>
+         <button v-on:click.prevent= 'display' >Click Me</button>
+        
+         </form>
+        
+         <div v-if='isVisible' id='box1'>
+         <p>{{FirstName}} {{LastName}} {{Age}}</p>
+         
+         </div>
+         
+         
+         `,
+         
+         data: function() {
+            return{
+             FirstName:'',
+             LastName:'',
+             Age:0,
+             isVisible:false
+            }
+         },
+         methods:{
+            display(){
+                if(this.Age > 17) {
+                    this.isVisible = true;
+                }else {
+                    this.isVisible = false;
+                }
+               
+            }
+         }
+    //everything in here appears in the div with the app id
+    });
+    //NEED TO MOUNT IT
+    app.mount('#app');
