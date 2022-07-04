ee2a305584c0427dbfbe3b1cbffd29e84b6b95a3
ikhlaas21 <ikhlaas21.rawoot@gmail.com>
2022-07-04 13:14:44 +0200

    3

---
+++ Student/script/code.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data: function(){
+        return{
+            FirstName:'Ikhlaas',
+            LastName: 'Rawoot',
+            Age: 19
+
+        }
+    }
+
+});
+app.mount('#app');
